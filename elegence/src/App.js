66eacdb2cab1036388b2742c8c977d3f8bdb329b
Vivek Patel <vp133202@gmail.com>
2023-02-24 04:08:58 +0530

    day-3 work

--- elegence/src/App.js
+++ elegence/src/App.js
@@ -4,12 +4,19 @@ import ProductCard from './components/Products/ProductCard';
 import ProductList from './components/Products/ProductList';
 import ProductPage from './components/Products/ProductPage';
 import MainRoutes from './components/MainRoutes';
+import Basket from './VPages/Cart/Basket';
+import Cart from './VPages/Cart/Cart';
+import Checkout from './VPages/Checkout/Checkout';
+import Pay from './VPages/Payment/Pay';
 
 function App() {
   return (
     <div className="App">
       <br/>
       <MainRoutes/>
+      <Cart/>
+      <Checkout/>
+      <Pay/>
     </div>
   );
 }
