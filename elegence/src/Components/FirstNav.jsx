2518c00897ffffcd5855724481fac6b1d1271bd8
unknown <laxmi choudhari>
2023-02-23 11:23:20 +0530

    work added

---
+++ elegence/src/Components/FirstNav.jsx
@@ -0,0 +1,45 @@
+import {
+    Box,
+    Flex,
+    Button,
+    Stack,
+    useColorModeValue,
+} from '@chakra-ui/react';
+import Account from './Account';
+
+
+export default function FirstNav() {
+    return (
+        <Box>
+            <Flex
+                bg={useColorModeValue('gray.100')}
+                py={{ base: 2 }}
+                px={{ base: 4 }}
+                borderBottom={1}
+                borderStyle={'solid'}
+                borderColor={useColorModeValue('gray.200', 'gray.900')}
+            >
+                <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+                </Flex>
+                <Account />
+                <Stack
+                    flex={{ base: 1, md: 0 }}
+                    justify={'flex-end'}
+                    pr={"20px"}
+                    direction={'row'}
+                    spacing={6}>
+                    {/* w={20} */}
+                    <Button
+                        mt={1}
+                        as={'a'}
+                        h={"20px"}
+                        fontSize={'sm'}
+                        fontWeight={400}
+                        href={'#'}>
+                        Sign In / Sign Up
+                    </Button>
+                </Stack>
+            </Flex>
+        </Box>
+    );
+}
---
+++ elegence/src/Components/Navbar.jsx
@@ -0,0 +1,37 @@
+import logo from "./Images/logo.png"
+import {
+  Box,
+  Flex,
+  HStack,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import FirstNav from './FirstNav';
+
+
+
+export default function Navbar() {
+
+
+  return (
+    <>
+      <FirstNav />
+      <Box pl={7} border={"1px solid #dcdcdc"} bg={useColorModeValue('white', 'gray.100')} w={"100%"}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <HStack alignItems={'center'}>
+            <Box cursor={"Pointer"} w={"180px"} border={"1px solid #dcdcdc"}>
+              <img src={logo} />
+            </Box>
+            <Box cursor={"Pointer"} w={"180px"} h={16} padding={15} border={"1px solid #dcdcdc"}>
+              <img src="https://images.ctfassets.net/5de70he6op10/y1O9tEDOvCGliGUvT9RoJ/eb8ab4734c5e3778b8b61da70ed441ac/AL_NewLogo_2.svg" />
+            </Box>
+          </HStack>
+          <Flex alignItems={'center'}>
+
+          </Flex>
+        </Flex>
+      </Box>
+
+      
+    </>
+  );
+}
