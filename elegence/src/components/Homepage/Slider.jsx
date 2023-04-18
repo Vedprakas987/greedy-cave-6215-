bfa15cfc005bf0f177be21798d4b1afb329811a6
Vedprakash Sinha <vedprakash4gs@gmail.com>
2023-04-18 20:20:32 +0530

    added new

--- elegence/src/components/Homepage/Slider.jsx
+++ elegence/src/components/Homepage/Slider.jsx
@@ -4,6 +4,7 @@ import "./slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import style from "../Products/styles/home.module.css"
 import { Box, Container, Img, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 
 
@@ -30,6 +31,7 @@ export function SliderTopCompanies(props) {
                 paddingY="8px"
                 fontSize={"12px"}
               >{item.title}</Text>
+              <Link to = "/Products">
               <Text
                     className={style.hiddenDiv}
                 textAlign={'center'}
@@ -38,6 +40,7 @@ export function SliderTopCompanies(props) {
                 fontSize={"12px"}
                 fontWeight="300"
               >Quick Shop</Text>
+                            </Link>
             </Box>
           </Container>
         })}
